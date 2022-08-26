61999e98a683936282ba346083f18f10445ae9c1
Richard <richard.herew@gmail.com>
2022-08-26 15:47:25 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fiber-mongo-api/configs"
+	"fiber-mongo-api/routes"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	configs.ConnectDB()
+
+	routes.UserRoute(app)
+
+	app.Listen(":6000")
+}
